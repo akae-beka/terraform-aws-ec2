a3d77b7dcea91f67f500d868d79e5e76178531b2
lbgc <lbrealdeveloper@gmail.com>
2020-05-25 13:49:23 +0200

    feat: add vpc_security_group_ids argument in module aws_ec2

--- main.tf
+++ main.tf
@@ -14,9 +14,10 @@ terraform {
 module "aws_ec2" {
   source = "./modules/ec2"
 
-  create_ec2    = var.create
-  instances     = var.instances
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = var.subnet_id
+  create_ec2             = var.create
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  instances              = var.instances
+  instance_type          = var.instance_type
+  vpc_security_group_ids = var.security_group_ids
 }
